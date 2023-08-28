598cf9c4e011c509193d50c9ee8f2866db6b861d
Pablo Bendinelli <pablo.bendinelli@gmail.com>
2023-08-28 11:07:57 -0300

    Add initial structure

---
+++ src/main/java/com/pbendinelli/devtest/similarproductservice/SimilarProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pbendinelli.devtest.similarproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbendinelli/devtest/similarproductservice/SimilarProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbendinelli.devtest.similarproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
